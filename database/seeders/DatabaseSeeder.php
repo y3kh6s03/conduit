bae0fb5c823a72a690d618057011a11a1721c7bc
yosuke026 <y3kh6s03@gmail.com>
2024-02-09 22:33:33 +0900

    create seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory(5)->create();
+        \App\Models\Tag::factory(10)->create();
+        \App\Models\Comment::factory(100)->create();
+        \App\Models\Like::factory(300)->create();
+        \App\Models\LikeCount::factory(1000)->create();
+        \App\Models\Post::factory(100)->create();
+        \App\Models\PostTag::factory(20)->create();
     }
 }
